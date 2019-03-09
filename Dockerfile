b6991d52bad859c3d303b876fe88084692c49558
Alexei Kornienko <alexei.kornienko@raccoongang.com>
2019-03-09 22:33:50 +0200

    Added posibility to load SSH key to container
    We need to provide correct SSH key and ansible Vault key to run
    playbooks properly.
    This commit adds bootstrap script that will be used to load needed keys
    from environment variables.
    We'll configure this variables in CI
    
    * TODO load Vault key properly

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,17 @@ FROM python:3
 
 RUN pip install ansible
 
-ADD playbooks /etc/ansible
+RUN adduser --system --home /home/ansible --disabled-password  --group ansible
+
+USER ansible
+WORKDIR /home/ansible
+
+RUN mkdir .ssh
+
+COPY --chown=ansible bootstrap.sh .
+
+COPY --chown=ansible playbooks /home/ansible
+
+ENV ANSIBLE_ROLES_PATH /home/ansible/roles
+
+ENTRYPOINT ["./bootstrap.sh"]
---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$ANSIBLE_SSH_KEY" ]
+then
+    echo "ANSIBLE_SSH_KEY env variable is needed to use image."
+    echo "By default it's provided by Gitlab CI."
+    echo "If you want to use this image locally please provide it yourself"
+    exit 1
+fi
+
+if [ -z "$ANSIBLE_VAULT_KEY" ]
+then
+    echo "ANSIBLE_VAULT_KEY env variable is needed to use image."
+    echo "By default it's provided by Gitlab CI."
+    echo "If you want to use this image locally please provide it yourself"
+    exit 1
+fi
+
+echo "$ANSIBLE_SSH_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+echo "Loaded SSH key from environment"
+
+echo "Loaded Ansible Vault key from environment"
+
+exec "$@"
